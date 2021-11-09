65ee9828354dc5b61e19b31b2b5cea072a3b4504
pr0gram1st <pr0gram1st@users.noreply.github.com>
2021-11-09 22:54:53 +0600

    Third Commit

--- main.cpp
+++ main.cpp
@@ -15,5 +15,26 @@ int main() {
     else{
         cout << pos << endl;
     }
+    ifstream file;
+    file.open("/Users/adiletkemelkhan/CLionProjects/assignment7/Task2.txt");
+    ofstream outfile1 ("Thread_1.txt");
+    ofstream outfile2 ("Thread_2.txt");
+    double *arr = new double[1002];
+    double ave = 0;
+    for(int i = 0; i < 100; ++i){
+        file >> arr[i];
+        ave += arr[i];
+    }
+    ave /= 100;
+    for(int i = 0; i < 100; ++i){
+        if(arr[i] - ave > 0){
+            outfile1 << arr[i] << "\n";
+        }
+        else{
+            outfile2 << arr[i] << "\n";
+        }
+    }
+    outfile1.close();
+    outfile2.close();
     return 0;
 }
